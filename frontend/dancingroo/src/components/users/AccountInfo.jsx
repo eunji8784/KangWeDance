9bc73488a457e621d13833677eaa097d7460a085
yiyou00@naver.com <yiyou00@naver.com>
2023-03-14 11:04:30 +0900

    [FE] FEAT: userpage - 아이정보 수정  목업 구현(프로필 이미지 제외)

--- frontend/dancingroo/src/components/users/AccountInfo.jsx
+++ frontend/dancingroo/src/components/users/AccountInfo.jsx
@@ -6,7 +6,6 @@ import {FaEdit} from 'react-icons/fa';
 const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
-    align-items: center;
 `;
 
 const Title = styled.div`
@@ -14,21 +13,33 @@ const Title = styled.div`
     align-items: center;
     font-size: 1.6rem;
     font-weight: 600;
+    margin-bottom: 0.1rem;
 `;
 
 const Experience = styled.div`
-    /* height: 0%;
-    width: ; */
-    /* font-weight: ${props=>props.color}; */
+    width: 12rem;
+    height: 1.5rem;
+    border: 0.1rem solid #F05475;
+    border-radius: 1rem;
+`;
+
+const ExperiencePercentage = styled.div`
+    width:${props=>props.gauge}%;
+    height: 1.5rem;
+    background-color: #F05475;
+    border-radius: 0.5rem;
 `;
 
 function AccountInfo(props) {
     const navigate = useNavigate();
-    const [experience, setExperience] = useState(10);
+    const [experiencePercentage, setExperiencePercentage] = useState(50);
+
     return (
         <Wrapper>
             <Title>유경이네 <FaEdit size={18} color="#F05475"/></Title>
-            {/* <Experience gauge={experience}/> */}
+            <Experience>
+                <ExperiencePercentage gauge={experiencePercentage}/>
+            </Experience>
         </Wrapper>
     );
 }
