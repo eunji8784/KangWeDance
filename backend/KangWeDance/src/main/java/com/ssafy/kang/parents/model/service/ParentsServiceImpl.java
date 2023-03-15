93d7cb9c5e119bdffae4b05adb160a9161a30d89
abin7989 <abin7989@gmail.com>
2023-03-15 17:41:40 +0900

    [BE] Fix: gradle 빌드 오류

--- backend/KangWeDance/src/main/java/com/ssafy/kang/parents/model/service/ParentsServiceImpl.java
+++ backend/KangWeDance/src/main/java/com/ssafy/kang/parents/model/service/ParentsServiceImpl.java
@@ -40,6 +40,31 @@ public class ParentsServiceImpl implements ParentsService {
 		}
 		return rt;
 	}
+
+	@Override
+	public Map<String, String> getNaverToken(String code) throws Exception {
+		String NAVER_CLIENT_ID = "GBp5LyAN_0Wl9eQyjVmb";
+		String NAVER_CLIENT_SECRET = "wGkyPOs6Y7";
+		String reqURL = "https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id="+NAVER_CLIENT_ID+"&client_secret="+NAVER_CLIENT_SECRET+"&code="+code;
+		Map<String, String> rt = new HashMap<>();
+		try {
+			URL url = new URL(reqURL);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			// 결과 코드가 200이라면 성공
+			int responseCode = conn.getResponseCode();
+			String result = naverUtil.readBody(conn.getInputStream());
+			System.out.println(result);
+			Gson gson = new Gson();
+			Map<String, Object> map = gson.fromJson(result, Map.class);
+			rt.put("access_token", (String) map.get("access_token"));
+			rt.put("refresh_token", (String) map.get("refresh_token"));
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return rt;
+	}
 	@Override
 	public Map<String, String> getUserInfo(String access_token) throws IOException {
 		String host = "https://kapi.kakao.com/v2/user/me";
@@ -76,35 +101,10 @@ public class ParentsServiceImpl implements ParentsService {
 		System.out.println(naver_account);
 		result.put("id", naver_account.get("id").toString());
 		result.put("nickname", naver_account.get("nickname").toString());
-		//result.put("nickname", properties.get("nickname").toString());
 
         return result;
 	}
 
-	@Override
-	public Map<String, String> getNaverToken(String code) throws Exception {
-		String NAVER_CLIENT_ID = "GBp5LyAN_0Wl9eQyjVmb";
-		String NAVER_CLIENT_SECRET = "wGkyPOs6Y7";
-		String reqURL = "https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id="+NAVER_CLIENT_ID+"&client_secret="+NAVER_CLIENT_SECRET+"&code="+code;
-		Map<String, String> rt = new HashMap<>();
-		try {
-			URL url = new URL(reqURL);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setRequestMethod("GET");
-			// 결과 코드가 200이라면 성공
-			int responseCode = conn.getResponseCode();
-			String result = naverUtil.readBody(conn.getInputStream());
-			System.out.println(result);
-			Gson gson = new Gson();
-			Map<String, Object> map = gson.fromJson(result, Map.class);
-			rt.put("access_token", (String) map.get("access_token"));
-			rt.put("refresh_token", (String) map.get("refresh_token"));
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return rt;
-	}
 	
 
 }
