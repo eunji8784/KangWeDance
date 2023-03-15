e3784661f3292a460737a19a34e7d98c81382110
SeoYong <swqq31@gmail.com>
2023-03-15 17:52:16 +0900

    [FE] Feat: 스테이지페이지 카운트기능 S08P22A606-164

--- frontend/dancingroo/src/pages/StagePage.jsx
+++ frontend/dancingroo/src/pages/StagePage.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { 
+    // useNavigate, 
+    useParams } from "react-router-dom";
 import styled from "styled-components";
-import DanceMode from "../components/modes/DanceMode";
+import DanceMode from "../components/stage/DanceMode";
 
 const Wrapper = styled.div`
     display: flex;
@@ -11,7 +13,7 @@ const Wrapper = styled.div`
 `;
 
 function StagePage(props) {
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
     const params = useParams();
     return (
         <Wrapper>
