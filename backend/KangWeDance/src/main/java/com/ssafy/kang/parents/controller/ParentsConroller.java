61002a37ca2ed6ce2e95c365a2e54197afc82a6f
abin7989 <abin7989@gmail.com>
2023-03-15 17:41:40 +0900

    [BE] Fix: gradle 빌드 오류

--- backend/KangWeDance/src/main/java/com/ssafy/kang/parents/controller/ParentsConroller.java
+++ backend/KangWeDance/src/main/java/com/ssafy/kang/parents/controller/ParentsConroller.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.ssafy.kang.common.dto.ApiResponse;
+import com.ssafy.kang.parents.model.ParentsDto;
 import com.ssafy.kang.parents.model.service.ParentsService;
+import com.ssafy.kang.util.JwtUtil;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,13 +23,25 @@ import lombok.RequiredArgsConstructor;
 public class ParentsConroller {
 	@Autowired
 	ParentsService parentsService;
+	private final JwtUtil jwtUtil;
+	public ParentsConroller() {
+		this.jwtUtil = new JwtUtil();
+	}
 	@GetMapping("/social/kakao")
-	public ApiResponse<?> userAdd(@RequestParam String code){
+	public ApiResponse<?> kakaoUserAdd(@RequestParam String code){
 
 		Map<String, String> token;
 		Map<String, String> userIO;
+		ParentsDto dto = new ParentsDto();
 		try {
 			token = parentsService.getToken(code);
+			userIO = parentsService.getUserInfo(token.get("access_token"));
+			dto.setSocailUid(userIO.get("id"));
+			dto.setSocailUid("Kakao");
+			dto.setNickname(userIO.get("nickname"));
+			//dto = parentsService.findSocial(dto.getSocailUid());
+			
+			
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
