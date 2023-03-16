410055f19aa95152a310d39ff1911ba9f2468294
김민식 <abin7989@gmail.com>
2023-03-16 14:26:50 +0900

    jwt구현및회원api

--- backend/KangWeDance/src/main/java/com/ssafy/kang/parents/model/service/ParentsService.java
+++ backend/KangWeDance/src/main/java/com/ssafy/kang/parents/model/service/ParentsService.java
@@ -3,9 +3,17 @@ package com.ssafy.kang.parents.model.service;
 import java.io.IOException;
 import java.util.Map;
 
+import com.ssafy.kang.parents.model.ParentsDto;
+
 public interface ParentsService {
 	public Map<String, String> getToken(String code) throws Exception;
 	public Map<String, String> getUserInfo(String access_token) throws IOException;
 	public Map<String, String> getNaverUserInfo(String token) throws Exception;
 	public Map<String, String> getNaverToken(String code) throws Exception;
+	public int findExperience(int idx) throws Exception;
+	void modifyNickname(ParentsDto parentsDto) throws Exception;
+	void removeUser(int idx) throws Exception;
+	
+	
+	
 }
