6213abd8a71685ccbc904ddb833961d09e914281
Minji Park <pmj7755@naver.com>
2023-03-16 10:40:13 +0900

    [BE] Feat: photos API 구현 (카카오 공유 제외) S08P22A606-141

--- backend/KangWeDance/src/main/java/com/ssafy/kang/photos/model/PhotosDto.java
+++ backend/KangWeDance/src/main/java/com/ssafy/kang/photos/model/PhotosDto.java
@@ -12,15 +12,11 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PhotosDto {
-	int songIdx;
-	int playMode; // 0: 댄스모드 / 1:카운트모드 / 2:타임모드 / 3: 레이싱모드
-	int difficulty;// 난이도
-	int calorieConsumption; // 칼로리 소모량
+	int photoIdx;
+	int parentIdx;
 
-	String title; // 노래 제목
-	String thumbnailUrl; // 썸네일 이미지
-	String videoUrl; // 동영상 URL
-	String previewMusicUrl; // 미리듣기 URL
-	String tag; // 해시태그
+	String photoImageUrl; // 사진 경로
+	String createDate;// 날짜
+	String photoName;// 사진파일명
 
 }
