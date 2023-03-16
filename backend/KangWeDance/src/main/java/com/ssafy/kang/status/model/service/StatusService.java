9cfa9f11aef33d06a839237185c0c716297936df
Minji Park <pmj7755@naver.com>
2023-03-16 15:09:31 +0900

    [BE] Feat: status API 구현 - 운동 기록조회 S08P22A606-136

---
+++ backend/KangWeDance/src/main/java/com/ssafy/kang/status/model/service/StatusService.java
@@ -0,0 +1,23 @@
+package com.ssafy.kang.status.model.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.ssafy.kang.play.model.PlayRecordDto;
+
+@Service
+public interface StatusService {
+//	| findOrder() | 조회 유형의 service 메서드 |
+//	| addOrder() | 등록 유형의 service 메서드 |
+//	| modifyOrder() | 변경 유형의 service 메서드 |
+//	| removeOrder() | 삭제 유형의 service 메서드 |
+//	| saveOrder() | 등록/수정/삭제 가 동시에 일어나는 유형의 service 메서드 |
+
+	// 아이별 운동 기록 조회
+	public List<PlayRecordDto> findplayRecord(int childIdx) throws Exception;
+
+	// 신체 변화 기록 조회
+
+	// 음식 검색
+}
