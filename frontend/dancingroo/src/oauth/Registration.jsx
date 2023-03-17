65b8666c567d92dd42c2a7a38b57ceaf64ad30f4
신동찬 <windcj1@naver.com>
2023-03-17 13:53:45 +0900

    FE폴더 master에 병합

---
+++ frontend/dancingroo/src/oauth/Registration.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { Wrapper, Main, Header, Footer, Section, Article } from "../components/common/ui/Semantics";
+
+function Registration(props) {
+    const navigate = useNavigate();
+    return (
+        <Wrapper>
+            
+        </Wrapper>
+    );
+}
+
+export default Registration;
