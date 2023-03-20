47e1213a2cd6808e769db0949c7c272565ce3f66
abin7989 <abin7989@gmail.com>
2023-03-20 14:19:43 +0900

    [BE] Feat: 크로스 오리진 문제 config 추가

---
+++ backend/KangWeDance/src/main/java/com/ssafy/kang/config/WebConfiguration.java
@@ -0,0 +1,47 @@
+package com.ssafy.kang.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+import org.springframework.beans.factory.annotation.Value;
+
+@Configuration
+@EnableWebMvc
+public class WebConfiguration implements WebMvcConfigurer{
+	
+	private final String uploadFilePath;
+
+	public WebConfiguration(@Value("${file.path.upload-files}") String uploadFilePath) {
+		this.uploadFilePath = uploadFilePath;
+	}
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+//		System.out.println("CORS Setting");
+//		default 설정.
+//		Allow all origins.
+//		Allow "simple" methods GET, HEAD and POST.
+//		Allow all headers.
+//		Set max age to 1800 seconds (30 minutes).
+		registry.addMapping("/**").allowedOrigins("*")
+//		.allowedOrigins("http://localhost:8080", "http://localhost:8081")
+			.allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
+					HttpMethod.DELETE.name(), HttpMethod.HEAD.name(), HttpMethod.OPTIONS.name(),
+					HttpMethod.PATCH.name())
+//			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")
+			.maxAge(1800);
+	}
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/swagger-ui.html**").addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+	
+}
+
