117bb90a7c8260f80d188554f4a4f042f1e7f2b5
chanshin0 <windcj1@naver.com>
2023-03-21 10:08:05 +0900

    [Feat] : 1.카카오로그인 처리로직 구현 2. 기존유저 여부 확인 후 분기처리 3. 메인페이지 디자인 수정 4. 에러페이지 추가

--- frontend/dancingroo/src/hooks/auth/useLogin.jsx
+++ frontend/dancingroo/src/hooks/auth/useLogin.jsx
@@ -6,31 +6,32 @@ import { useNavigate } from 'react-router-dom';
 const useLogin = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [, setCookie] = useCookies(['access_token']);
+  const [, setCookie] = useCookies(['accessToken']);
+  const [isUser, setIsUser] = useState(null)
 
-  const history = window.history
+  const navigate = useNavigate()
 
   const handleLogin = async (Oauth, code) => {
     setLoading(true);
     setError(null);
     try {
       const response = await useApi.get(`/parents/social/${Oauth}?code=${code}`);
-      console.log(response)
-      const { access_token } = response.data.accessToken;
-      setCookie('access_token', access_token, { path: '/' }); //모든 경로에서 토큰 허용하겠다)
+      if (response.data.status!==200){
+        alert(`로그인 실패, status:${response.data.status}`)
+        navigate('/')
+      } 
+      const { accessToken, isUser } = response.data.data;
+      console.log(accessToken, isUser)
+      setCookie('accessToken', accessToken, { path: '/' }); //모든 경로에서 토큰 허용하겠다)
       setLoading(false);
-      if (response.isUser) {
-        history.replace('/play')
-      } else {
-        history.replace('/signup')
-      }
+      setIsUser(isUser)
     } catch (error) {
       setLoading(false);
       setError(error.message);
     }
   };
 
-  return { loading, error, handleLogin };
+  return { loading, error, handleLogin, isUser };
 };
 
 export default useLogin;
