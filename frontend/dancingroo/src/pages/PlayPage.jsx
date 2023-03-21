45fc8c1945dd846d35a94d9c605e84f1e57a0dbf
SeoYong <swqq31@gmail.com>
2023-03-21 15:07:54 +0900

    Feat: 스테이지페이지 댄스모드 기본틀 완성 재커밋  S08P22A606-235

---
+++ frontend/dancingroo/src/pages/PlayPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+// import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+function PlayPage(props) {
+    // const navigate = useNavigate();
+    return (
+        <Wrapper>
+            PlayPage
+        </Wrapper>
+    );
+}
+
+export default PlayPage;
