b023887988afa85742e051e126328ce033f7a7bf
윤서용 <swqq31@gmail.com>
2023-03-23 16:24:05 +0900

    스테이지페이지 카운트모드 구현

--- frontend/dancingroo/src/components/stage/Feedback.jsx
+++ frontend/dancingroo/src/components/stage/Feedback.jsx
@@ -3,17 +3,18 @@ import styled from "styled-components"
 import greatImg from "../../assets/images/great.png"
 import goodImg from "../../assets/images/good.png"
 import cheerupImg from "../../assets/images/cheerup.png"
+import readyImg from "../../assets/images/ready.png"
+import goImg from "../../assets/images/go.png"
 
 const Wrapper = styled.div`
   width: 100%;
   display: flex;
+  position: relative;
   justify-content: center;
-  img {
-  width: 25%;
-  margin-top: 1rem;
-  height: auto;
-  }
   .popup {
+    width: 25%;
+    margin-top: 1rem;
+    height: auto;
     animation: pop-up 1s;
     @keyframes pop-up {
       0% {
@@ -39,13 +40,70 @@ const Wrapper = styled.div`
       }
     }
   }
+  .ready {
+    position: absolute;
+    margin-top: 4rem;
+    width: 30%;
+    animation: ready-animation 2s ease-in-out forwards;
+  }
+
+  @keyframes ready-animation {
+    0% {
+      opacity: 0.5;
+      transform: translateY(-30%);
+    }
+    30% {
+      opacity: 1;
+      transform: translateY(0%)
+    }
+    80% {
+      opacity: 1;
+      transform: scale(1);
+    }
+    100% {
+      opacity: 0;
+      transform: scale(1.1);
+    }
+  }
+  .go {
+    position: absolute;
+    margin-top: 3.5rem;
+    width: 35%;
+    animation: go-animation 1s ease-in-out both;
+    animation-delay: 2s;
+  }
+  @keyframes go-animation {
+    0% {
+      opacity: 0;
+      transform: scale(1.5);
+    }
+    20% {
+      opacity: 1;
+      transform: scale(1);
+    }
+    40% {
+      opacity: 1;
+      transform: scale(1);
+    }
+    100% {
+      opacity: 0;
+      transform: scale(1.2);
+    }
+  }
+ 
 `
-function Feedback({showGreat, showGood, showCheerUp}) {
+function Feedback({showGreat, showGood, showCheerUp, showReadyGo}) {
   return (
     <Wrapper>
       {showGreat && <img className="popup" src={greatImg} alt="great"/>}
       {showGood && <img className="popup" src={goodImg} alt="good"/>}
       {showCheerUp && <img className="popup" src={cheerupImg} alt="cheerup"/>}
+      {showReadyGo && 
+        <>
+          <img className="ready" src={readyImg} alt="ready"/>
+          <img className="go" src={goImg} alt="go"/>
+        </>
+      }
     </Wrapper>
   )
 }
