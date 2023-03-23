d2584bfa34a5f512d2253a6d36d04426c46431b4
신동찬 <windcj1@naver.com>
2023-03-23 13:08:21 +0900

    로그인/로그아웃 상태 관리 로직 구현

---
+++ frontend/dancingroo/src/hooks/auth/useLogout.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import { useCookies } from 'react-cookie';
+import { useDispatch } from 'react-redux';
+import { logout } from '../../store/userSlice';
+
+const useLogout = () => {
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [cookie, , removeCookie] = useCookies('accessToken');
+
+  const handleLogout = async () => {
+    removeCookie('accessToken', { path: '/' }); // Remove the accessToken cookie
+    dispatch(logout());
+    try {
+      const baseURL = "https://kangwedance.site/dev"
+      const response = await fetch(baseURL + '/parents/logout', {
+        method: "GET",
+        headers: {
+          'Content-Type': 'application/json',
+          accesstoken: cookie.accessToken,
+        },
+      })
+      const json = await response.json();
+    } catch (error) {
+      setError(error.message); 
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { loading, error, handleLogout };
+};
+
+export default useLogout;
