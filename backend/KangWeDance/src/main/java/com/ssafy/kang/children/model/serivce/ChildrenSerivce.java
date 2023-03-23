6eab3464dd6c2d862cdd2eaca16615a9e5b5333e
abin7989 <abin7989@gmail.com>
2023-03-23 12:53:30 +0900

    [BE] Feat: 프로필 이미지 uRL 반환 API 추가 S08P22A606-244

--- backend/KangWeDance/src/main/java/com/ssafy/kang/children/model/serivce/ChildrenSerivce.java
+++ backend/KangWeDance/src/main/java/com/ssafy/kang/children/model/serivce/ChildrenSerivce.java
@@ -3,6 +3,7 @@ package com.ssafy.kang.children.model.serivce;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ssafy.kang.children.model.BodyRecordDto;
 import com.ssafy.kang.children.model.ChildrenDto;
@@ -18,5 +19,7 @@ public interface ChildrenSerivce {
 	void addChildrenBody(BodyRecordDto bodyRecordDto) throws Exception;
 
 	List<ChildrenDto> findChildren(int accesstoken)throws Exception;
+
+	String findProfileUrl(MultipartFile file)throws Exception;
 
 }
