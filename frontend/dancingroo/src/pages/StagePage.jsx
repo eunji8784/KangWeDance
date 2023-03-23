1442e2e77b0c8b585b580c531c0fdb34c77b91d0
윤서용 <swqq31@gmail.com>
2023-03-23 16:24:05 +0900

    스테이지페이지 카운트모드 구현

--- frontend/dancingroo/src/pages/StagePage.jsx
+++ frontend/dancingroo/src/pages/StagePage.jsx
@@ -4,6 +4,7 @@ import {
     useParams } from "react-router-dom";
 import styled from "styled-components";
 import DanceMode from "../components/stage/DanceMode";
+import CountMode from "../components/stage/CountMode";
 
 const Wrapper = styled.div`
     display: flex;
@@ -18,6 +19,7 @@ function StagePage(props) {
     return (
         <Wrapper>
           {params.modeId === "0" && <DanceMode/>}
+          {params.modeId === "1" && <CountMode/>}
         </Wrapper>
     );
 }
