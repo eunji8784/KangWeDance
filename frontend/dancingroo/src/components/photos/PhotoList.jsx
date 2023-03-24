4f44807b839fd5d382a83f01a4a1f968ab2f1081
이유경 <yiyou00@naver.com>
2023-03-24 16:08:53 +0900

    포토 페이지 사진 삭제&사진/꾸미기 선택안했을때 화면

--- frontend/dancingroo/src/components/photos/PhotoList.jsx
+++ frontend/dancingroo/src/components/photos/PhotoList.jsx
@@ -1,33 +1,51 @@
 import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 import { Wrapper } from "../common/ui/Semantics";
 import PhotoItem from "./PhotoItem"
 
+
+const Empty = styled.div`
+    height: 7.2rem;
+    width: 12.8rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1rem;
+`;
+
 function PhotoList({handleImge, photoList}) {
     const [newDay, setNewDay] = useState([]);
-    const [date, setDate] = useState("");
     
     //스티커랑 프레임 리스트
     useEffect(()=>{
-        if (photoList) {
+        if (photoList.length !== 0) {
             var arr = [];
+            var date = '';
             for (var i = 0; i < photoList.length; i++) {
-                if(date !== photoList[i].createDate){
+                if(date != photoList[i].createDate){
                     arr.push(true);
-                    setDate(photoList[i].createDate);
-                    // console.log(newDay) 
+                    date = photoList[i].createDate;
                 } else{
                     arr.push(false);
                 }
             }
-            setNewDay(arr);
+            setNewDay(arr) 
         }
+
     },[photoList])
 
     return (
         <Wrapper>
-            {photoList?.map((photo, index) => {
-                return <PhotoItem handleImge={handleImge} key={index} date={photo.createDate} imgUrl={photo.photoImageUrl} newDay={newDay[index]}/>;
-            })}
+            {photoList?
+                photoList.map((photo, index) => {
+                return <PhotoItem handleImge={handleImge} key={index} date={photo.createDate} imgUrl={photo.photoImageUrl} newDay={newDay[index]} 
+                photoIdx = {photo.photoIdx}/>;
+                })
+                :
+                <Empty>
+                    아직 사진이 없어요!
+                </Empty>
+            }
         </Wrapper>
     );
 }
