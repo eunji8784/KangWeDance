234dfb76539c00293ad14f94d00e699d336768fe
박민지 <pmj7755@naver.com>
2023-03-24 16:45:03 +0900

    하이브큐엘 테스트 -2

--- backend/KangWeDance/src/main/java/com/ssafy/kang/common/SuccessCode.java
+++ backend/KangWeDance/src/main/java/com/ssafy/kang/common/SuccessCode.java
@@ -13,23 +13,24 @@ public enum SuccessCode {
 	/*
 	 * 등록/생성: CREATE, 조회: READ, 수정: UPDATE, 삭제/취소: DELETE
 	 */
-	//회원
+	// 회원
 	READ_EXPERIENCE(SUCCESS, "회원 경험치 조회 성공"), UPDATE_NICKNAME(SUCCESS, "회원 닉네임 변경 성공"),
-	DELETE_USER(SUCCESS, "회원 삭제 성공"),LOGOUT(SUCCESS,"로그아웃 성공"),
-	LOGIN(SUCCESS,"로그인 성공"),GO_JOIN(SUCCESS,"회원 가입"),
-	//아이
-	CREATE_CHILDREN(SUCCESS,"아이 등록 성공"),DELETE_CHILDREN(SUCCESS,"아이 삭제 성공"),
-	READ_PROFILEURL(SUCCESS,"프로필 URL 조회 성공"),
-	UPDATE_CHILDREN(SUCCESS,"아이 수정 성공"),CREATE_BODY_CHILDREN(SUCCESS,"아이 신체정보 성공"),
-	READ_CHILDREN(SUCCESS,"아이 조회 성공"),
-	//사진
+	DELETE_USER(SUCCESS, "회원 삭제 성공"), LOGOUT(SUCCESS, "로그아웃 성공"), LOGIN(SUCCESS, "로그인 성공"), GO_JOIN(SUCCESS, "회원 가입"),
+	// 아이
+	CREATE_CHILDREN(SUCCESS, "아이 등록 성공"), DELETE_CHILDREN(SUCCESS, "아이 삭제 성공"),
+	READ_PROFILEURL(SUCCESS, "프로필 URL 조회 성공"), UPDATE_CHILDREN(SUCCESS, "아이 수정 성공"),
+	CREATE_BODY_CHILDREN(SUCCESS, "아이 신체정보 성공"), READ_CHILDREN(SUCCESS, "아이 조회 성공"),
+	// 사진
 	CREATE_PHOTO(SUCCESS, "사진 등록 성공"), READ_PHOTO_LIST(SUCCESS, "사진 리스트 조회 성공"),
 	READ_PRAME_LIST(SUCCESS, "스티커 및 프레임 조회 성공"), CREATE_KAKAO(SUCCESS, "카카오톡 공유하기 성공"),
 	DELETE_PHOTO(SUCCESS, "사진 삭제하기 성공"),SHARE_PHOTO(SUCCESS,"사진 공유 URI 생성 성공"),
 	// play
 	READ_PLAY_LIST(SUCCESS, "플레이 목록 조회 성공"), CREATE_PLAY_RESULT(SUCCESS, "플레이 결과 등록 성공"),
 	// status
-	READ_PLAYRECORD_LIST(SUCCESS, "운동 기록 조회 성공"), READ_FOOD_LIST(SUCCESS, "음식 검색 성공");
+	READ_PLAYRECORD_LIST(SUCCESS, "운동 기록 조회 성공"), READ_FOOD_LIST(SUCCESS, "음식 검색 성공"),
+
+	// hadoop(hive)
+	CREATE_BODYRECORD(SUCCESS, "신체 기록 하둡에 업로드 성공"), READ_BODYRECORD(SUCCESS, "신체 기록 하둡에서 불러오기 성공");
 
 	private final StatusCode statusCode;
 	private final String message;
