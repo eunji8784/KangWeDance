c75c9c27a5289969bd4fd505ebf98e377e560d7c
chanshin0 <windcj1@naver.com>
2023-03-26 19:35:55 +0900

    가족닉네임 수정 api 연결

--- frontend/dancingroo/src/store/userSlice.js
+++ frontend/dancingroo/src/store/userSlice.js
@@ -68,9 +68,12 @@ const userSlice = createSlice({
       if (state.children[selectedIdx].childIdx===null) state.addChild = true
       else state.addChild = false
       console.log(state.select,'번째 아이 플레이 중!', '아이등록중?', state.addChild)
+    },
+    editFamilyname(state, action){
+      state.familyname = action.payload
     }
   },
 })
 
-export const {login, logout, getChildState, patchChildState, childSelect} = userSlice.actions
+export const {login, logout, getChildState, patchChildState, childSelect, editFamilyname} = userSlice.actions
 export default userSlice.reducer
