c5ad343b6738f7c406cb1b5bd27fd33cd201f583
chanshin0 <windcj1@naver.com>
2023-03-27 10:10:57 +0900

    [FE] : 오류 픽스

--- frontend/dancingroo/src/pages/PlayPage.jsx
+++ frontend/dancingroo/src/pages/PlayPage.jsx
@@ -19,21 +19,24 @@ const Wrapper = styled.div`
 function PlayPage({handleWatchingPage}) {
     // 동찬 추가
     const navigate = useNavigate()
-    const firstChild = useSelector(state=>state.userState.children[0].childIdx)
+    // const firstChild = useSelector(state=>state.userState.children[0].childIdx)
     //
     const {data:playData, isLoading, error, fetchApi:playListApi} = useApi()
     // const {recommendedData, recommendedIsLoading, recommendedError, recommendedApi} = useApi('/play/recommendation')
 
     // 동찬 추가) 아이 조회해서 등록된 아이가 0명이면, join페이지로 보내기
-    useEffect(()=>{
-        if (firstChild===null) navigate('/users/join')
-    },[firstChild])
+    // useEffect(()=>{
+    //     if (firstChild===null){
+    //         // alert(firstChild)
+    //         // navigate('/users/join')
+    //     } 
+        
+    // },[firstChild])
     //
     useEffect(()=>{
         handleWatchingPage('play')
         playListApi('GET', '/play')
     },[])
-
     return (
         <Wrapper>
             <DanceSection danceData={playData?.data.filter((e)=>e.playMode===0)} />
