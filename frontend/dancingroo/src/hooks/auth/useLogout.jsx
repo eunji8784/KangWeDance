e59c95049f66fc8e9487ee3f63009b3a97eecaad
신동찬 <windcj1@naver.com>
2023-03-27 16:41:23 +0900

    소셜 로그인, 로그아웃 기능 완성

--- frontend/dancingroo/src/hooks/auth/useLogout.jsx
+++ frontend/dancingroo/src/hooks/auth/useLogout.jsx
@@ -5,6 +5,7 @@ import { logout } from '../../store/userSlice';
 
 const useLogout = () => {
   const dispatch = useDispatch();
+  const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [cookie, , removeCookie] = useCookies('accessToken');
@@ -23,6 +24,8 @@ const useLogout = () => {
       })
       if (!response.ok) throw new Error(`HTTP error: ${response.status}`)
       const json = await response.json();
+      console.log(json)
+      setData(json)
     } catch (error) {
       setError(error.message); 
     } finally {
@@ -30,7 +33,7 @@ const useLogout = () => {
     }
   };
 
-  return { loading, error, handleLogout };
+  return { data, loading, error, handleLogout };
 };
 
 export default useLogout;
