4e7188e750d2ab6b23a20564b67da00d895901ce
SeoYong <swqq31@gmail.com>
2023-03-27 16:45:06 +0900

    [FE] Feat: 플레이페이지 추천 API S08P22A606-353

--- frontend/dancingroo/src/components/play/DanceSection.jsx
+++ frontend/dancingroo/src/components/play/DanceSection.jsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import PlayItem from "./PlayItem";
-import {Wrapper, H1} from "../common/ui/Semantics";
+import { Wrapper, H1 } from "../common/ui/Semantics";
 
 const DanceWrapper = styled(Wrapper)`
   width: 100%;
   display: grid;
   grid-template-columns: repeat(${props => props.numColumns}, 1fr);
-  gap: 10px;
+  gap: 2rem;
 `
 
 function DanceSection({danceData}) {
 
-  const numColumns = danceData?.length;
+  const [numColumns, setNumColumns] = useState(2);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      if (width >= 768) {
+        setNumColumns(2);
+      } else {
+        setNumColumns(1);
+      }
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <>
