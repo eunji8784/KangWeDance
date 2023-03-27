93d56d5b0bfc887066c3af93a966812048754759
윤서용 <swqq31@gmail.com>
2023-03-27 17:48:08 +0900

    스테이지페이지 API

--- frontend/dancingroo/src/components/play/GameSection.jsx
+++ frontend/dancingroo/src/components/play/GameSection.jsx
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import PlayItem from "./PlayItem";
-import {Wrapper, H1} from "../common/ui/Semantics";
+import { Wrapper, H1 } from "../common/ui/Semantics";
 
 const GameWrapper = styled(Wrapper)`
   width: 100%;
   display: grid;
   grid-template-columns: repeat(${props => props.numColumns}, 1fr);
-  gap: 10px;
+  gap: 1rem;
 `
 
 function GameSection({gameData}) {
 
-  const numColumns = gameData?.length;
+  const [numColumns, setNumColumns] = useState(4);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      if (width >= 1200) {
+        setNumColumns(3);
+      } else if (width >= 768) {
+        setNumColumns(2);
+      } else {
+        setNumColumns(1);
+      }
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <>
       <H1>으쌰으쌰 놀이</H1>
       <GameWrapper numColumns={numColumns}>
-          {gameData?.map((item) => <PlayItem key={item.songIdx} item={item} />)}
+          {gameData?.map((item) => <PlayItem key={item.songIdx} item={item} tags={true}/>)}
       </GameWrapper>
     </>
   );
