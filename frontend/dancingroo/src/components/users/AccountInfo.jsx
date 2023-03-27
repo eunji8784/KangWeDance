886035e40a8e30ca1dc79b7c0489bcb2f7ffac0a
chanshin0 <windcj1@naver.com>
2023-03-27 13:27:58 +0900

    [FE] Feat : api연결 및 레벨디자인 객체 추가

--- frontend/dancingroo/src/components/users/AccountInfo.jsx
+++ frontend/dancingroo/src/components/users/AccountInfo.jsx
@@ -1,11 +1,13 @@
 import React, {useState} from "react";
 // import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import {FaEdit} from 'react-icons/fa';
 import { useSelector,useDispatch } from "react-redux";
+import Modal from "../common/ui/Modal";
 import { editFamilyname } from "../../store/userSlice";
 import useApi from "../../hooks/auth/useApi";
 import { useEffect } from "react";
+import { levelDesign } from "../../utils/levelDesign";
+
 const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
@@ -17,9 +19,17 @@ const Title = styled.div`
     font-weight: 600;
     margin-bottom: 0.1rem;
     cursor: pointer;
+    position:relative;
+    justify-content:center;
 `;
 const Input = styled.input`
-  /* display: none;  */
+    outline:none;
+    border:1px solid transparent;
+    height:2rem;
+    font-size:1.5rem;
+    font-weight:bold;
+    margin-bottom:0.5rem;
+    left:0;
 `;
 const Experience = styled.div`
     width: 12rem;
@@ -38,54 +48,48 @@ function AccountInfo(props) {
     // const navigate = useNavigate();
     const dispatch = useDispatch()
     const familyname = useSelector(state=>state.userState.familyname)
-    const [isEditing, setIsEditing] = useState(false);
-    const [newFamilyname, setNewFamilyname] = useState(familyname);
-    const [experiencePercentage, setExperiencePercentage] = useState(50);
-    const editFamilyname = useApi()
+    const [newFamilyname, setNewFamilyname] = useState(familyname||'');
+    const [experiencePercentage, setExperiencePercentage] = useState(30);
+    const patchFamilyname = useApi()
     const getExp = useApi()
+
 
     // 경험치 조회 요청
     useEffect(()=>{
         const onSuccess = (json)=>{
-            // 응답으로 경험치퍼센티지 계산한 뒤, 인자로 넣기
-            setExperiencePercentage('here')
+            const {level, experience} = json.data
+            const neededExp = levelDesign[level+1]
+            let percentExp = Math.round(experience/neededExp)
+            setExperiencePercentage(percentExp)
         }
         getExp.fetchApi('GET', '/parents/experience-score', onSuccess) // [1]. 콜백을 3번째로 넣어도 body로 안가고 콜백함수로 판단함.
     },[])
 
     // 가족닉네임 변경 요청
-    const editFamilynameHandler = ()=>{
-        const onSuccess = ()=>{
-            dispatch(editFamilyname(newFamilyname))
+    const editFamilynameHandler = (e)=>{
+        if (e.key!=="Enter") return
+
+        if (window.confirm('가족닉네임 변경할까요?')){
+            const onSuccess = ()=>{
+                dispatch(editFamilyname(newFamilyname))
+            }
+            patchFamilyname.fetchApi('PATCH', '/parents/nickname', {nickname:newFamilyname}, onSuccess) // [2]. 4개 인자 모두 사용한 경우
         }
-        editFamilyname.fetchApi('PATCH', '/parents/nickname', newFamilyname, onSuccess) // [2]. 4개 인자 모두 사용한 경우
     }
-    const handleInputBlur = () => {
-        setIsEditing(false); 
-    };
     const handleInputChange = (e) => {
         setNewFamilyname(e.target.value); 
-    };
-    const handleTitleClick = () => {
-        setIsEditing(true);
     };
     return (
         <Wrapper>
             <Title>
-                {isEditing ?
                 <Input
                 type="text"
                 value={newFamilyname}
                 onChange={handleInputChange}
-                onBlur={handleInputBlur}
-                autoFocus
-                onClick={handleTitleClick}
-                />
-                :
-                familyname
-                }
-                <FaEdit size={18} color="#F05475"
-                onClick={editFamilynameHandler}
+                width={newFamilyname?.length+1 || 5}
+                maxLength={15}
+                onKeyPress={editFamilynameHandler}
+                placeholder={familyname}
                 />
             </Title>
             <Experience>
