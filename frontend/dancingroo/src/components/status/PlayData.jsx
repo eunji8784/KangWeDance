1d755fbbdc528cca8b367d397a2c6494955e1ec3
chanshin0 <windcj1@naver.com>
2023-03-27 17:57:11 +0900

    1. 통계 페이지 타이틀 출력 2. 기본이미지 url 수정 3. 통계페이지 모달 css 수정 4. 유저페이지 이미지업로드 api 테스트

--- frontend/dancingroo/src/components/status/PlayData.jsx
+++ frontend/dancingroo/src/components/status/PlayData.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 // import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
 import PlayCalendar from "../common/ui/PlayCalendar";
 import { ModalBtn } from "./HealthData";
@@ -25,6 +26,9 @@ const Wrapper = styled.div`
       //[1-1] section 중 header라는 class를 가진 태그선택자
       &.header{
         justify-content:flex-start;
+        span{
+          font-size:1.1rem;
+        }
       }
       // [1-2] section 중 main이라는 class를 가진 태그선택자
       &.main{
@@ -72,6 +76,8 @@ const Table = styled.table`
 `;
 
 function PlayData({handleIsModalOpen, username}) {
+  const selected = useSelector(state=>state.userState.select)
+  const selectedChild = useSelector(state=>state.userState.children[selected||0]) 
     const [selectedDay, setSelectedDay] = useState()
     const handleSelectedDay = (date)=>{
         setSelectedDay(date)
@@ -79,7 +85,7 @@ function PlayData({handleIsModalOpen, username}) {
     return (
         <Wrapper>
             <section className="section header">
-                <h3>{username}(이)는 이번 달 총 OO회 운동했어요</h3>
+                <h3>{selectedChild.nickname}<span>(이)는</span> 이번 달 총 OO<span>회</span> 운동했어요</h3>
                 <ModalBtn onClick={()=>handleIsModalOpen()}>오늘의 정보 입력</ModalBtn>
             </section>
             <section className="section main">
