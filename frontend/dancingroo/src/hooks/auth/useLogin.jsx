71dd7953ad3bfdc405bdb1332d8afc86dcbf8cbe
chanshin0 <windcj1@naver.com>
2023-03-28 15:05:07 +0900

    [FE] Feat : 토큰만료 확인 및 처리로직 구현

--- frontend/dancingroo/src/hooks/auth/useLogin.jsx
+++ frontend/dancingroo/src/hooks/auth/useLogin.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react';
 import { useCookies } from 'react-cookie';
 import { useDispatch } from 'react-redux';
-import { login } from '../../store/userSlice';
+import { login,logout } from '../../store/userSlice';
+import { useNavigate } from 'react-router-dom';
 
 const useLogin = () => {
   const dispatch = useDispatch()
+  const navigate = useNavigate();
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [, setCookie] = useCookies('accessToken');
+  const [, removeCookie, setCookie] = useCookies('accessToken');
 
   const handleSocialLogin = async (Oauth, code) => {
     try {
@@ -28,6 +30,16 @@ const useLogin = () => {
       console.log(accessToken, isUser)
       setCookie('accessToken', accessToken, { path: '/' }); //모든 경로에서 토큰 허용하겠다)
       dispatch(login(accessToken))
+      
+      // 만료된 토큰이면 로그아웃 시키도록 (만료기간:하루)
+      const expiryTime = 1000*60*60*24
+      setTimeout(()=>{
+        removeCookie('accessToken', { path: '/' });
+        dispatch(logout());
+        alert('토큰이 만료되었습니다. 로그아웃됩니다.')
+        navigate('/')
+      }, expiryTime)
+
     } catch (error) {
       setError(error.message); 
     } finally {
