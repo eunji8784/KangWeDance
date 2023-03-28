0124740d1f6a019571fd37ae618a508d05c8762c
yiyou00@naver.com <yiyou00@naver.com>
2023-03-28 15:25:34 +0900

    [FE] Feat: 포토페이지 사진 삭제시 얼랏창 S08P22A606-327

--- frontend/dancingroo/src/store/index.js
+++ frontend/dancingroo/src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit"
 import userSlice from "./userSlice"
 import stageSlice from "./stageSlice"
+import photoSlice from "./photoSlice"
 
 const store = configureStore({
   reducer: {
     userState: userSlice,
     stage: stageSlice,
+    photo: photoSlice,
   },
 })
 
