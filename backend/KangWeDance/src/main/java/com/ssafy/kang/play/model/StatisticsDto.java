9006b0a7abd339614245e04ab08fc4f6925ca246
Minji Park <pmj7755@naver.com>
2023-03-30 09:23:28 +0900

    [BE] Feat: 하둡 API insert / update 통계 처리 API 구현 S08P22A606-297

---
+++ backend/KangWeDance/src/main/java/com/ssafy/kang/play/model/StatisticsDto.java
@@ -0,0 +1,21 @@
+package com.ssafy.kang.play.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class StatisticsDto {
+	int arm;
+	int leg;
+	int flexibility;
+	int body;
+	int aerobic;
+	long playTime; // 초단위
+}
