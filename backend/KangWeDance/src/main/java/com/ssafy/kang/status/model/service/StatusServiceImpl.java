267538be4feab0c46b3bc00cc4f7d10515a591b9
eunjikim8784 <eunjikim8784@gmail.com>
2023-03-31 14:21:28 +0900

    [BE] Feat: 체중, 신장 백분율 환산 구현 S08P22A606-349

--- backend/KangWeDance/src/main/java/com/ssafy/kang/status/model/service/StatusServiceImpl.java
+++ backend/KangWeDance/src/main/java/com/ssafy/kang/status/model/service/StatusServiceImpl.java
@@ -10,6 +10,7 @@ import com.ssafy.kang.children.model.BodyRecordDto;
 import com.ssafy.kang.play.model.PlayRecordDto;
 import com.ssafy.kang.status.model.BodyTagDto;
 import com.ssafy.kang.status.model.FoodsDto;
+import com.ssafy.kang.status.model.PercentileDto;
 import com.ssafy.kang.status.model.mapper.StatusMapper;
 
 import lombok.RequiredArgsConstructor;
@@ -35,15 +36,67 @@ public class StatusServiceImpl implements StatusService {
 	public List<PlayRecordDto> findMonthlyRecord(int childIdx, int month) throws Exception {
 		return sqlSession.getMapper(StatusMapper.class).selectMonthlyRecord(childIdx, month);
 	}
-	
+
 	@Override
-	public List<BodyRecordDto> findRecordList(int childIdx)  throws Exception {
+	public List<BodyRecordDto> findRecordList(int childIdx) throws Exception {
 		return sqlSession.getMapper(StatusMapper.class).selectBodyRecordList(childIdx);
 	}
 
 	@Override
 	public List<BodyTagDto> findBodyTagRecord(int parentIdx) throws Exception {
 		return sqlSession.getMapper(StatusMapper.class).selectBodyTagRecord(parentIdx);
+	}
+
+	@Override
+	public int findWeightPercentile(int childIdx) throws Exception {
+		PercentileDto percentileDto = sqlSession.getMapper(StatusMapper.class).selectWeightPercentile(childIdx);
+		int weightPercentile = 0;
+		if (percentileDto == null) {
+			return weightPercentile;
+		}
+		int childWeight = percentileDto.getWeight();
+		int min = Integer.MAX_VALUE;
+		int[] arr1 = { 1, 3, 5, 10, 15, 25, 50, 75, 85, 90, 95, 97, 99 };
+		int[] arr2 = { percentileDto.getPercentile_1th(), percentileDto.getPercentile_3th(),
+				percentileDto.getPercentile_5th(), percentileDto.getPercentile_10th(),
+				percentileDto.getPercentile_15th(), percentileDto.getPercentile_25th(),
+				percentileDto.getPercentile_50th(), percentileDto.getPercentile_75th(),
+				percentileDto.getPercentile_85th(), percentileDto.getPercentile_90th(),
+				percentileDto.getPercentile_95th(), percentileDto.getPercentile_97th(),
+				percentileDto.getPercentile_99th() };
+		for (int i = 0; i < arr1.length; i++) {
+			if (Math.abs(childWeight - arr2[i]) < min) {
+				min = Math.abs(childWeight - arr2[i]);
+				weightPercentile = arr1[i];
+			}
+		}
+		return 100 - weightPercentile;
+	}
+	
+	@Override
+	public int findHeightPercentile(int childIdx) throws Exception {
+		PercentileDto percentileDto = sqlSession.getMapper(StatusMapper.class).selectHeightPercentile(childIdx);
+		int heightPercentile = 0;
+		if (percentileDto == null) {
+			return heightPercentile;
+		}
+		int childHeight = percentileDto.getHeight();
+		int min = Integer.MAX_VALUE;
+		int[] arr1 = { 1, 3, 5, 10, 15, 25, 50, 75, 85, 90, 95, 97, 99 };
+		int[] arr2 = { percentileDto.getPercentile_1th(), percentileDto.getPercentile_3th(),
+				percentileDto.getPercentile_5th(), percentileDto.getPercentile_10th(),
+				percentileDto.getPercentile_15th(), percentileDto.getPercentile_25th(),
+				percentileDto.getPercentile_50th(), percentileDto.getPercentile_75th(),
+				percentileDto.getPercentile_85th(), percentileDto.getPercentile_90th(),
+				percentileDto.getPercentile_95th(), percentileDto.getPercentile_97th(),
+				percentileDto.getPercentile_99th() };
+		for (int i = 0; i < arr1.length; i++) {
+			if (Math.abs(childHeight - arr2[i]) < min) {
+				min = Math.abs(childHeight - arr2[i]);
+				heightPercentile = arr1[i];
+			}
+		}
+		return 100 - heightPercentile;
 	}
 
 	// 일회성 코드임
