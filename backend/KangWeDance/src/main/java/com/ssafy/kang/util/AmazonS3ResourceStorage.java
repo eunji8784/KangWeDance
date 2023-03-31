844791442e60df7ed7b6dbb4ff5354b532c3f67e
김민식 <abin7989@gmail.com>
2023-03-31 10:18:49 +0900

    포토 api 수정

--- backend/KangWeDance/src/main/java/com/ssafy/kang/util/AmazonS3ResourceStorage.java
+++ backend/KangWeDance/src/main/java/com/ssafy/kang/util/AmazonS3ResourceStorage.java
@@ -1,6 +1,5 @@
 package com.ssafy.kang.util;
 
-
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
@@ -16,41 +15,41 @@ import java.io.File;
 @ConditionalOnProperty(prefix = "cloud.aws.s3", name = "bucket")
 @RequiredArgsConstructor
 public class AmazonS3ResourceStorage {
-    @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
-    private final AmazonS3Client amazonS3Client;
+	@Value("${cloud.aws.s3.bucket}")
+	private String bucket;
+	private final AmazonS3Client amazonS3Client;
 
-    public void store(String fullPath, MultipartFile multipartFile) {
-    	System.out.println(fullPath);
-        File file = new File(MultipartUtil.getLocalHomeDirectory(), fullPath);
-        try {
-            multipartFile.transferTo(file);
-            amazonS3Client.putObject(new PutObjectRequest(bucket, fullPath, file)
-                    .withCannedAcl(CannedAccessControlList.PublicRead));
-        } catch (Exception e) {
-            throw new RuntimeException();
-        } finally {
-            if (file.exists()) {
-                file.delete();
-            }
-        }
-    }
-
-	public String getUrl(String fullPath, MultipartFile multipartFile)  {
+	public void store(String fullPath, MultipartFile multipartFile) {
+		System.out.println(fullPath);
 		File file = new File(MultipartUtil.getLocalHomeDirectory(), fullPath);
-        try {
-            multipartFile.transferTo(file);
-            amazonS3Client.putObject(new PutObjectRequest(bucket, fullPath, file)
-                    .withCannedAcl(CannedAccessControlList.PublicRead));
-            String makeurl = amazonS3Client.getUrl(bucket, fullPath).toString();
-            makeurl = "https://d3qb4vbeyp8phu.cloudfront.net/"+makeurl.substring(52,makeurl.length());
-            return makeurl;
-        } catch (Exception e) {
-            throw new RuntimeException();
-        } finally {
-            if (file.exists()) {
-                file.delete();
-            }
-        }
+		try {
+			multipartFile.transferTo(file);
+			amazonS3Client.putObject(
+					new PutObjectRequest(bucket, fullPath, file).withCannedAcl(CannedAccessControlList.PublicRead));
+		} catch (Exception e) {
+			throw new RuntimeException();
+		} finally {
+			if (file.exists()) {
+				file.delete();
+			}
+		}
+	}
+
+	public String getUrl(String fullPath, MultipartFile multipartFile) {
+		File file = new File(MultipartUtil.getLocalHomeDirectory(), fullPath);
+		try {
+			multipartFile.transferTo(file);
+			amazonS3Client.putObject(
+					new PutObjectRequest(bucket, fullPath, file).withCannedAcl(CannedAccessControlList.PublicRead));
+			String makeurl = amazonS3Client.getUrl(bucket, fullPath).toString();
+			makeurl = "https://d3qb4vbeyp8phu.cloudfront.net/" + makeurl.substring(52, makeurl.length());
+			return makeurl;
+		} catch (Exception e) {
+			throw new RuntimeException();
+		} finally {
+			if (file.exists()) {
+				file.delete();
+			}
+		}
 	}
 }
