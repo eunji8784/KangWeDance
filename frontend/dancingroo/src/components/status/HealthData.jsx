d9027ef617a3176d449abccce70bd2132626f654
chanshin0 <windcj1@naver.com>
2023-04-01 13:50:59 +0900

    건강기록페이지 예외처리 추가

--- frontend/dancingroo/src/components/status/HealthData.jsx
+++ frontend/dancingroo/src/components/status/HealthData.jsx
@@ -182,7 +182,7 @@ function HealthData(props) {
       } 
       getBodyChanges.fetchApi('GET', '/status/body-changes', onBodyResSuccess)
       const onTagResSuccess = (response)=>{
-        if (response.data===[]) return
+        if (response.data===[] || response.data[selected]===[] || !response.data[selected]) return
         const data = response.data[selected]
         const sortedData = [...Object.entries(data)].filter(ele=>ele[0]!=='childIdx' && ele[0]!=='총_플레이시간').sort((a,b)=>a[1]-b[1])
         setSortedTagList(sortedData)
