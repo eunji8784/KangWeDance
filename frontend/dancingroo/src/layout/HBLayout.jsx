3f492212d8ccc33ad1843c9d4de98261add57646
yiyou00@naver.com <yiyou00@naver.com>
2023-04-03 17:23:57 +0900

    [FE] Fix: 운동기록 안내문구 추가 & 포토페이지 디자인 수정 S08P22A606-370

--- frontend/dancingroo/src/layout/HBLayout.jsx
+++ frontend/dancingroo/src/layout/HBLayout.jsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router-dom";
 // import { useSelector } from "react-redux";
 // import TopBar from "../components/common/nav/TopBar";
 import HeaderBar from "../components/common/nav/HeaderBar";
-import BottomBar from "../components/common/nav/BottomBar";
+import FooterBar from "../components/common/nav/FooterBar";
 import styled from "styled-components";
 import ChildProfile from "../components/common/ui/ChildProfile";
 import NoteCompo from "../components/common/effects/NoteCompo";
@@ -16,12 +16,14 @@ const Bodysuit = styled.div`
     justify-content: space-between;
     /* justify-items: center; */
     height: 100vh;
-    /* min-height: calc(100vh- 500px); */
     /* position: relative; */
     // @ 양 옆 마진
     /* padding-top: 8rem; */
     margin:0 15% !important;
     /* border: 1px solid blue; */
+    position: relative;
+    min-height: 100vh;
+    padding-bottom: 5rem;
 `;
 
 const OutletWrapper = styled(Outlet)`
@@ -41,14 +43,15 @@ const HBLayout = (props) => {
     // }, [isLoggedIn])
 
     return(
-        <Bodysuit>
-            {/* <TopBar/> */}
-            <HeaderBar watchingPage={watchingPage}/>
-            <ChildProfile/>
-            <OutletWrapper />
-            <NoteCompo/>
-            {/* <BottomBar/> */}
-        </Bodysuit>
+        <>
+            <Bodysuit>
+                <HeaderBar watchingPage={watchingPage}/>
+                <ChildProfile/>
+                <NoteCompo/>
+                <OutletWrapper />
+            </Bodysuit>
+            {/* <FooterBar/>      */}
+        </>
     )
 
 }
