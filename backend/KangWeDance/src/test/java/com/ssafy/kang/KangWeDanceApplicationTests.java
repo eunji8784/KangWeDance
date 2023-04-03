3fe71017bee1b3c3a261a7216379244ddb19d4c4
abin7989 <abin7989@gmail.com>
2023-04-03 14:18:09 +0900

    [BE] Feat: test케이스 회원가입 S08P22A606-403

--- backend/KangWeDance/src/test/java/com/ssafy/kang/KangWeDanceApplicationTests.java
+++ backend/KangWeDance/src/test/java/com/ssafy/kang/KangWeDanceApplicationTests.java
@@ -5,15 +5,28 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.ssafy.kang.common.SuccessCode;
+import com.ssafy.kang.parents.controller.ParentsConroller;
+import com.ssafy.kang.parents.model.ParentsDto;
+import com.ssafy.kang.util.JwtUtil;
 
 @SpringBootTest
 @TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
 class KangWeDanceApplicationTests {
 
+	String accessToken = "eyJ0eXAiOiJKV1QiLCJyZWdEYXRlIjoxNjgwNDk4MjA1Nzc4LCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2ODA1ODQ2MDUsInN1YiI6ImFjY2Vzcy10b2tlbiIsInVzZXJpZHgiOjI0fQ.PlfL6bsT5CMgMUbQPqdRplcqyOaOaIuW6vPVFXVBLdQ";
+	
+	@Autowired
+	ParentsConroller parentsConroller;
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void Join() {
+		JwtUtil jwtUtil = new JwtUtil();
+		ParentsDto pd = new ParentsDto();
+		pd.setFamilyname("ssafy123");	
+		assertEquals(parentsConroller.nicknameModify(accessToken,pd).getMessage(),SuccessCode.UPDATE_NICKNAME.getMessage());
 	}
 
 }
