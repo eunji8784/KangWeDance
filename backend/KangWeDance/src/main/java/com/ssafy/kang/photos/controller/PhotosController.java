df4330de9f3003582c84736f696e68e07ab3f2d0
박민지 <pmj7755@naver.com>
2023-04-03 12:23:20 +0900

    Update PhotosController.java

--- backend/KangWeDance/src/main/java/com/ssafy/kang/photos/controller/PhotosController.java
+++ backend/KangWeDance/src/main/java/com/ssafy/kang/photos/controller/PhotosController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -55,11 +54,6 @@ public class PhotosController {
 			int parentIdx = jwtService.getUserIdx(accesstoken);
 			photosService.addUpdate(file, parentIdx);
 
-
-	public ApiResponse<?> photosAdd(@RequestHeader("accesstoken") String accesstoken,@RequestBody PhotosDto photosDto) throws Exception {
-		try {
-			photosDto.setParentIdx(jwtService.getUserIdx(accesstoken));
-			photosService.addUpdate(photosDto);
 			return ApiResponse.success(SuccessCode.CREATE_PHOTO);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -125,7 +119,7 @@ public class PhotosController {
 			List<FramesDto> frameDto = photosService.findFrames(level);
 			List<FramesDto> stickerDto = photosService.findStickers();
 
-			for (int i = 0; i < level&& i<5; i++) {
+			for (int i = 0; i < level; i++) {
 				frameDto.get(i).setUnLock(true);
 			}
 
