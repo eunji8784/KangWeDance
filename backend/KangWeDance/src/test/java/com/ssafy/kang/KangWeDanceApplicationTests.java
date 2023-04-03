98f943eb7772b9863fac6ae819fe9e26b547b9e7
abin7989 <abin7989@gmail.com>
2023-04-03 12:47:16 +0900

    [BE] Fix: 프레임 mapper 수정 S08P22A606-403

---
+++ backend/KangWeDance/src/test/java/com/ssafy/kang/KangWeDanceApplicationTests.java
@@ -0,0 +1,19 @@
+package com.ssafy.kang;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
+class KangWeDanceApplicationTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
