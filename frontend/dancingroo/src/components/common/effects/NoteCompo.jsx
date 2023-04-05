b271f46f838e3662411672fb7079c2b6d684096f
chanshin0 <windcj1@naver.com>
2023-04-05 18:11:40 +0900

    [FE] Fix : 음표 애니메이션 추가

--- frontend/dancingroo/src/components/common/effects/NoteCompo.jsx
+++ frontend/dancingroo/src/components/common/effects/NoteCompo.jsx
@@ -39,53 +39,75 @@ const Note = styled.img`
 `;
 
 const NoteCompo = () => {
-    /* eslint-disable */
   const [positions, setPositions] = useState([]);
 
-  const notes = [
+  const generateNote = (min, max, index) => {
+    const srcOptions = [note1, note2, note3, note4, note5];
+    const src = srcOptions[Math.floor(Math.random() * srcOptions.length)];
+
+    let top, left, right;
+    do {
+      top = Math.floor(Math.random() * (max - min)) + min;
+      left = Math.floor(Math.random() * 10);
+      right = Math.floor(Math.random() * 10);
+    } while (
+      positions.some(
+        ({ prevTop, prevLeft, prevRight }) =>
+          Math.abs(prevTop - top) < 85 &&
+          Math.abs(prevLeft - left) < 80 &&
+          Math.abs(prevRight - right) < 80
+      )
+    );
+
+    const noteStyle = {
+      top: `${top}%`,
+      ...(index === 0 ? { left: `${left}%` } : { right: `${right}%` }),
+    };
+
+    return (
+      <Note
+        key={`${min}-${index}`}
+        style={noteStyle}
+        src={src}
+        onClick={() => handleClick(min, max, index)}
+      />
+    );
+  };
+
+  const handleClick = (min, max, index) => {
+    const updatedPositions = positions.filter(
+      ({ prevTop }) => !(prevTop >= min && prevTop < max)
+    );
+    setPositions([...updatedPositions, ...generatePositions(min, max, index)]);
+  };
+
+  const generatePositions = (min, max, index) => {
+    const { top, left, right } = generateNote(min, max, index).props.style;
+    return [{ prevTop: top, prevLeft: left, prevRight: right }];
+  };
+
+  useEffect(() => {
+    const newPositions = [];
+    notesConfig.forEach(({ min, max }) => {
+      newPositions.push(...generatePositions(min, max, 0));
+    });
+    setPositions(newPositions);
+  }, []);
+
+  const notesConfig = [
     { min: 20, max: 45 },
     { min: 45, max: 65 },
     { min: 65, max: 95 },
-  ].flatMap(({ min, max }) => {
+  ];
+
+  const notes = notesConfig.flatMap(({ min, max }, index) => {
     const count = positions.filter(
       ({ prevTop }) => prevTop >= min && prevTop < max
     ).length;
     return Array(1 + (count < 1 ? 1 : 0))
       .fill()
-      .map((_, index) => {
-        const srcOptions = [note1, note2, note3, note4, note5];
-        const src = srcOptions[Math.floor(Math.random() * srcOptions.length)];
-
-        let top, left, right;
-        do {
-          top = Math.floor(Math.random() * (max - min)) + min;
-          left = Math.floor(Math.random() * 10);
-          right = Math.floor(Math.random() * 10);
-        } while (
-          positions.some(
-            ({ prevTop, prevLeft, prevRight }) =>
-              Math.abs(prevTop - top) < 85 &&
-              Math.abs(prevLeft - left) < 80 &&
-              Math.abs(prevRight - right) < 80
-          )
-        );
-
-        const noteStyle = {
-          top: `${top}%`,
-          ...(index === 0 ? { left: `${left}%` } : { right: `${right}%` }),
-        };
-
-        return <Note key={`${min}-${index}`} style={noteStyle} src={src} />;
-      });
+      .map((_, index) => generateNote(min, max, index));
   });
-
-  useEffect(() => {
-    const newPositions = notes.map((note) => {
-      const { top, left, right } = note.props.style;
-      return { prevTop: top, prevLeft: left, prevRight: right };
-    });
-    setPositions(newPositions);
-  }, []);
 
   return <>{notes}</>;
 };
