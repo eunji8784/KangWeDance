25c1beb141585f91cfaa646e529f7019bb918855
SeoYong <swqq31@gmail.com>
2023-04-06 12:27:18 +0900

    Refactor: IOS 모바일 개선 S08P22A606-468

--- frontend/dancingroo/src/components/play/DanceSection.jsx
+++ frontend/dancingroo/src/components/play/DanceSection.jsx
@@ -1,41 +1,53 @@
-import React, { useEffect, useState } from "react";
+import React
+// , { useEffect, useState } 
+from "react";
 import styled from "styled-components";
 import PlayItem from "./PlayItem";
-import { Wrapper, H1 } from "../common/ui/Semantics";
-import dance from "../../assets/images/017.png"
+import { Wrapper } from "../common/ui/Semantics";
 import DescribeSection from "./DescribeSection"
+import dance from "../../assets/images/017.png"
 
 const DanceWrapper = styled(Wrapper)`
   width: 100%;
   display: grid;
-  grid-template-columns: repeat(${props => props.numColumns}, 1fr);
+  /* grid-template-columns: repeat(${props => props.numColumns}, 1fr); */
   gap: 5rem;
   margin-bottom: 8rem;
+
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: 767px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
 `
 
 function DanceSection({danceData}) {
 
-  const [numColumns, setNumColumns] = useState(2);
+  // const [numColumns, setNumColumns] = useState(2);
 
-  useEffect(() => {
-    const handleResize = () => {
-      const width = window.innerWidth;
-      if (width >= 768) {
-        setNumColumns(2);
-      } else {
-        setNumColumns(1);
-      }
-    };
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     const width = window.innerWidth;
+  //     if (width >= 768) {
+  //       setNumColumns(2);
+  //     } else {
+  //       setNumColumns(1);
+  //     }
+  //   };
+  //   handleResize();
+  //   window.addEventListener("resize", handleResize);
+  //   return () => window.removeEventListener("resize", handleResize);
+  // }, []);
 
   return (
     <>
     <DescribeSection imgUrl={dance} color="#FFFBEB" title="댄스 모드" 
     context="신나는 노래에 맞춰 댄스!"/>
-      <DanceWrapper numColumns={numColumns}>
+      <DanceWrapper
+      //  numColumns={numColumns}
+      >
         {danceData?.map((item) => <PlayItem key={item.songIdx} item={item} />)}
       </DanceWrapper>
     </>
--- frontend/dancingroo/src/components/play/GameSection.jsx
+++ frontend/dancingroo/src/components/play/GameSection.jsx
@@ -1,52 +1,67 @@
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-import PlayItem from "./PlayItem";
-import { Wrapper } from "../common/ui/Semantics";
-import DescribeSection from "./DescribeSection"
-
-import dance from "../../assets/images/018.png"
-
-
-const GameWrapper = styled(Wrapper)`
-  width: 100%;
-  display: grid;
-  grid-template-columns: repeat(${props => props.numColumns}, 1fr);
-  gap: 2rem;
-  margin-bottom: 8rem;
-`
+  import React
+  // , { useEffect, useState } 
+  from "react";
+  import styled from "styled-components";
+  import PlayItem from "./PlayItem";
+  import { Wrapper } from "../common/ui/Semantics";
+  import DescribeSection from "./DescribeSection"
+  import dance from "../../assets/images/018.png"
 
 
-function GameSection({gameData}) {
+  const GameWrapper = styled(Wrapper)`
+    width: 100%;
+    display: grid;
+    /* grid-template-columns: repeat(${props => props.numColumns}, 1fr); */
+    gap: 2rem;
+    margin-bottom: 8rem;
 
-  const [numColumns, setNumColumns] = useState(4);
+    @media (min-width: 1200px) {
+      grid-template-columns: repeat(3, 1fr);
+    }
 
-  useEffect(() => {
-    const handleResize = () => {
-      const width = window.innerWidth;
-      if (width >= 1200) {
-        setNumColumns(3);
-      } else if (width >= 768) {
-        setNumColumns(2);
-      } else {
-        setNumColumns(1);
-      }
-    };
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+    @media (min-width: 768px) and (max-width: 1199px) {
+      grid-template-columns: repeat(2, 1fr);
+    }
 
-  return (
-    <>
-      <DescribeSection imgUrl={dance} color="#ECF9FF" title="놀이 모드" 
-      context="정확한 자세를 유지해요!"
-      context2="빅데이터에 기반한 추천까지!"
-      />
-      <GameWrapper numColumns={numColumns}>
-          {gameData?.map((item) => <PlayItem key={item.songIdx} item={item} tags={true}/>)}
-      </GameWrapper>
-    </>
-  );
-}
+    @media (max-width: 767px) {
+      grid-template-columns: repeat(1, 1fr);
+    }
+  `
 
-export default GameSection;
+
+  function GameSection({gameData}) {
+
+    // const [numColumns, setNumColumns] = useState(4);
+
+    // useEffect(() => {
+    //   const handleResize = () => {
+    //     const width = window.innerWidth;
+    //     if (width >= 1200) {
+    //       setNumColumns(3);
+    //     } else if (width >= 768) {
+    //       setNumColumns(2);
+    //     } else {
+    //       setNumColumns(1);
+    //     }
+    //   };
+    //   handleResize();
+    //   window.addEventListener("resize", handleResize);
+    //   return () => window.removeEventListener("resize", handleResize);
+    // }, []);
+
+    return (
+      <>
+        <DescribeSection imgUrl={dance} color="#ECF9FF" title="놀이 모드" 
+        context="정확한 자세를 유지해요!"
+        context2="빅데이터에 기반한 추천까지!"
+        />
+        <GameWrapper
+        //  numColumns={numColumns}
+        >
+            {gameData?.map((item) => <PlayItem key={item.songIdx} item={item} tags={true}/>)}
+        </GameWrapper>
+      </>
+    );
+  }
+
+  export default GameSection;
