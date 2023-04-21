6c74b077513670d95699b4cd1397a41c48026c8e
chanshin0 <windcj1@naver.com>
2023-04-21 14:47:45 +0900

    error fix
    Former-commit-id: 7fb2f58ea0d066b8ed7648f36980e4e5d0ea6b1f

--- frontend/dancingroo/src/components/photos/RightArea.jsx
+++ frontend/dancingroo/src/components/photos/RightArea.jsx
@@ -25,7 +25,7 @@ const ButtonSection = styled(Wrapper)`
 `
 
 const Card = styled.div`
-    width : ${window.innerWidth/2.3};
+    width : ${window.innerWidth/2.2};
     height : ${window.innerWidth*0.2557};
     border: 0.1rem solid rgba(0, 0, 0, 0.1);
 `;
@@ -35,7 +35,7 @@ const BackGroungImage = ({ image }) => {
     return (
       <Image
         image={img}
-        width={window.innerWidth/2.3}
+        width={window.innerWidth/2.2}
         height={window.innerWidth*0.2557}
       />
     );
@@ -215,87 +215,6 @@ function RightArea({image, frameImage, stickerImage, stickerNum}) {
          }
     }, [shareImageUrl]);
 
-    //스티커 누르고 위치 변경할때
-    const checkDeselect = (e) => {
-        const clickedOnEmpty = e.target === e.target.getStage;
-        if (clickedOnEmpty) {
-            selectShape(null);
-        }
-    };
-
-    //이미지 다운로드
-   const downloadImage = () => {
-       const url = stageRef.current.toDataURL();
-       const today = new Date();
-       const year = today.getFullYear(); // 년도
-       const month = today.getMonth() + 1;  // 월
-       const date = today.getDate();  // 날짜
-   
-       downloadURI(url, 'KangWeDance'+ year + month + date);   
-   }
-
-    //이미지 uri 다운로드 함수
-    const downloadURI = (uri, name) => {
-        var link = document.createElement("a");
-        link.download = name;
-        link.href = uri;
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-        // delete link;
-      }
-
-    //이미지 공유하기
-    const shareImage = () =>{
-        getUrl();
-        kakaoShare();
-    }
-
-    //이미지 MultipartFile로 변경
-    const getImageUrl = useApi()
-    const getUrl = () => {
-        const base64 = stageRef.current.toDataURL();
-        fetch(base64)
-        .then(res => res.blob())
-        .then(blob => {
-          const fd = new FormData();
-          const file = new File([blob], "filename.png");
-          fd.append('image', file)
-          console.log("Dd")
-          console.log(fd)
-        //   getImageUrl.fetchApi('POST', `/children/profile`, {file:fd})
-        })
-    }
-
-    //카카오톡 공유하기
-    const kakaoShare = () => {
-        if (window.Kakao) {
-            const kakao = window.Kakao
-            if (!kakao.isInitialized()) {
-                kakao.init(process.env.REACT_APP_API_KEY_KAKAO)
-            }
-            
-            kakao.Share.sendDefault({
-                objectType: 'feed',
-                content: {
-                  title: '오늘의 기록',
-                  description: '캥위댄스에서 편지가 왔어요',
-                  imageUrl:
-                    'https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
-                },
-                buttons: [
-                  {
-                    title: '다운로드하기',
-                    link: {
-                      mobileWebUrl: 'https://developers.kakao.com',
-                      webUrl: 'https://developers.kakao.com',
-                    },
-                  },
-                ],
-            })
-         }
-    }
-
     return (
         <MainSection>
             <ButtonSection>
@@ -307,7 +226,7 @@ function RightArea({image, frameImage, stickerImage, stickerNum}) {
              onTouchStart={checkDeselect}
              >
                     <Stage
-                        width={window.innerWidth/2.3}
+                        width={window.innerWidth/2.2}
                         height={window.innerWidth*0.2557}
                         ref={stageRef}
                         >
